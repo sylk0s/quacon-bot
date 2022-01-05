11cc3d9c92298c879ab8e9c2f55ec471b2f37930
Sylk0s <sylkos49@gmail.com>
2022-01-04 20:36:32 -0500

    spreadsheet script

---
+++ spreadsheet.js
@@ -0,0 +1,71 @@
+// https://github.com/VLabStudio/Tutorials/tree/master/How to use Google Forms data in Node.js
+
+// Import dependencies
+const fs = require("fs");
+const { google } = require("googleapis");
+
+const service = google.sheets("v4");
+const credentials = require("./credentials.json");
+
+// Configure auth client
+const authClient = new google.auth.JWT(
+    credentials.client_email,
+    null,
+    credentials.private_key.replace(/\\n/g, "\n"),
+    ["https://www.googleapis.com/auth/spreadsheets"]
+);
+
+(async function () {
+    try {
+
+        // Authorize the client
+        const token = await authClient.authorize();
+
+        // Set the client credentials
+        authClient.setCredentials(token);
+
+        // Get the rows
+        const res = await service.spreadsheets.values.get({
+            auth: authClient,
+            spreadsheetId: "1m05DvWgB5R8lcghH2seIiKTuDpVTQkFDeWkDrvkCBkI",
+            range: "A:D",
+        });
+
+        // All of the answers
+        const answers = [];
+
+        // Set rows to equal the rows
+        const rows = res.data.values;
+
+        // Check if we have any data and if we do add it to our answers array
+        if (rows.length) {
+
+            // Remove the headers
+            rows.shift()
+
+            // For each row
+            for (const row of rows) {
+                answers.push({ timeStamp: row[0], answer: row[1], q1: row[2], q2: row[3] });
+            }
+
+        } else {
+            console.log("No data found.");  
+        }
+
+        // Saved the answers
+        fs.writeFileSync("answers.json", JSON.stringify(answers), function (err, file) {
+            if (err) throw err;
+            console.log("Saved!");
+        });
+
+    } catch (error) {
+
+        // Log the error
+        console.log(error);
+
+        // Exit the process with error
+        process.exit(1);
+
+    }
+
+})();
